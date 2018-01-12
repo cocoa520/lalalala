e138152ffaf54b6f04e397f0bc4a045761a28a14
iMobie-ZL <964664795@qq.com>
2018-01-12 17:14:26 +0800

    added note.m

---
+++ TestNew/note.m
@@ -0,0 +1,16 @@
+#######################
+########
+########解析CDB文件分别调用的文件的类的read方法顺序及其步骤
+1、_dataArray = [[NSMutableArray alloc] initWithArray:[_information getTrackArrayByMediaTypes:[IMBCommonEnum categoryNodeToMediaTyps:Category_Music]]];
+2、NSArray *trackArray = [self trackArray];
+3、[self refreshMedia];     IMBInformation   - trackArray
+4、[_mediaDatabase parse];  IMBInformation   - refreshMedia
+5、[self readDatabase:databaseRoot]; IBMusicDataBase  - parse
+6、[root read:iPod reader:reader currPosition:0];  IMBBaseDataBase  - readDatabase:
+7、decCurrPostion = [containerHeader read:iPod reader:decompressionData currPosition:decCurrPostion];    IMBItunesCDBRoot   - (long)read:(IMBiPod *)ipod reader:(NSData *)reader currPosition:(long)currPosition
+8、currPosition = [_childElement read:iPod reader:reader currPosition:currPosition];  IMBListContainerHeader  - (long)read:(IMBiPod *)ipod reader:(NSData *)reader currPosition:(long)currPosition
+9、currPosition = [_childSection read:iPod reader:reader currPosition:currPosition];
+   IMBListContainer  - (long)read:(IMBiPod *)ipod reader:(NSData *)reader currPosition:(long)currPosition
+10、currPosition = [track read:iPod reader:reader currPosition:currPosition];
+    IMBTrackList   - (long)read:(IMBiPod *)ipod reader:(NSData *)reader currPosition:(long)currPosition
+11、IMBTrack  - (long)read:(IMBiPod *)ipod reader:(NSData *)reader currPosition:(long)currPosition
